c560075ad9d1d77355248330fb49211b5302d2a2
nik h <nik0001@protonmail.com>
2024-04-27 23:31:53 -0400

    fetch rgb coords

--- rgb_coords.py
+++
@@ -1,9 +0,0 @@
-# File should open rgb image, segmented mask and depth mask.
-# Using segmented mask fetch the rgb cutout
-# make a rouch depth map of the rgb pixels
-
-import numpy as np
-import bpy
-import bmesh
-import math
-from mathutils import Matrix, Vector, Euler
